591be9bb5026957df66d9121ab408e894eba8674
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-04-03 13:52:59 +0530

    feat: add basic todo app

---
+++ src/components/TaskInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTask } from "../redux/actions";
+
+const TaskInput = () => {
+  const [taskText, setTaskText] = useState("");
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    setTaskText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskText.trim() !== "") {
+      dispatch(addTask(taskText));
+      setTaskText("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={taskText}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskInput;
